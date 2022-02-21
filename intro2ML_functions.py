0b57a71f33196185dda847e2d3581d4c5a5de200
bahareh368 <64007829+bahareh368@users.noreply.github.com>
2022-02-21 14:08:33 +0100

    Update intro2ML_functions.py

--- intro2ML_functions.py
+++ intro2ML_functions.py
@@ -56,6 +56,55 @@ def plot_descent_2d(g, weight_history, cost_history, trajectory = True):
     ax[0].set_title('Cost')
             
     plt.tight_layout() #this line ensures the axis labels don't overlap
+    
+    
+def standard_normalizer(x):
+    # compute the mean and standard deviation of the input
+    x_means = np.mean(x,axis = 1)[:,np.newaxis] # enter your code here
+    x_stds = np.std(x,axis = 1)[:,np.newaxis]  # enter your code here  
+
+    # check to make sure thta x_stds > small threshold because we can not devide by zero, 
+    # for those not, we divide by 1 instead of original standard deviation
+    ind = np.argwhere(x_stds < 10**(-2))
+    if len(ind) > 0:
+        ind = [v[0] for v in ind]
+        adjust = np.zeros((x_stds.shape))
+        adjust[ind] = 1.0
+        x_stds += adjust
+
+    # create standard normalizer function
+    normalizer = lambda data: (data - x_means)/x_stds # your code here
+
+    # create inverse standard normalizer
+    inverse_normalizer = lambda data: data*x_stds + x_means # your code here
+
+    # return normalizer 
+    return normalizer,inverse_normalizer
+
+from autograd import grad 
+
+# gradient descent function - inputs: g (input function), alpha (steplength parameter), max_its (maximum number of iterations),w
+def gradient_descent(g,alpha,max_its,w):
+    
+    # compute gradient module using autograd
+    gradient = grad(g)
+
+    # gradient descent loop
+    weight_history = [w]              # weight history container
+    cost_history = [g(w)]             # cost function history container
+    for k in range(max_its):
+        
+        # evaluate the gradient
+        grad_eval = gradient(w)
+
+        # take gradient descent step
+        w = w - alpha*grad_eval
+        
+        # record weight and cost
+        weight_history.append(w)
+        cost_history.append(g(w))
+        
+    return weight_history,cost_history
 
 
 
